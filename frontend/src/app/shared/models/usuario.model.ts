ad831d1d1a4b8455f6b412e8c4cb1e02877cd8aa
Qtaro-Kujo <joao_albertop@hotmail.com>
2025-08-31 19:05:57 -0300

    Revert "feat(R01): implementar login"
    This reverts commit 3be56a95b68ef0344b470f727abcf5dc52e8afd6.

--- frontend/src/app/shared/models/usuario.model.ts
+++
@@ -1,9 +0,0 @@
-export class Usuario {
-    constructor(
-        public id: number =0,
-        public nome: string = '',
-        public login: string = '',
-        public senha: string = '',
-        public perfil: string = ''
-    ){}
-    }
