29d9f352ca84240a62239f8e6d6e8f69b370a6ef
Mateus De Vita <mateusdevitata@gmail.com>
2025-08-31 20:56:18 -0300

    feat: prepara funcionamento do form de autocadastro

---
+++ frontend/src/app/services/model/endereco.ts
@@ -0,0 +1,9 @@
+export interface Endereco {
+    tipo: string; // rua, avenida, etc
+    logradouro: string;
+    numero: string;
+    complemento?: string;
+    cep: string;
+    cidade: string;
+    estado: string;
+}
