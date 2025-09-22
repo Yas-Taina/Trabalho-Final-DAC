f500c79d52e0ff7fa4ef92ebc7664f329be5658a
Mateus De Vita <mateusdevitata@gmail.com>
2025-09-21 23:45:59 -0300

    feat: implementa funcionamento da tela de melhores clientes

--- frontend/src/app/pages/gerente/melhores-clientes/melhores-clientes.component.ts
+++ frontend/src/app/pages/gerente/melhores-clientes/melhores-clientes.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DadosClienteResponse } from '../../../services';
+import { DadosClienteResponse, LocalClientesService } from '../../../services';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,66 +11,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './melhores-clientes.component.css'
 })
 export class MelhoresClientesComponent {
-  clientes: DadosClienteResponse[] = [
-    {
-      cpf: '111.111.111.11',
-      nome: 'Maria Terezinha',
-      email: 'mariazinha@gmail.com',
-      endereco: {
-        tipo: 'Avenida',
-        logradouro: 'Rua das Flores',
-        numero: '56',
-        complemento: 'ap09',
-        cep: '88888-888',
-        cidade: 'Curitiba',
-        estado: 'PR'
+
+  clienteService = inject(LocalClientesService);
+
+  clientes: DadosClienteResponse[] = [];
+
+  ngOnInit() {
+    this.clienteService.getClientes("melhores_clientes").subscribe({
+      next: (clientes) => {
+        this.clientes = clientes;
       },
-      conta: '2222',
-      saldo: '9999.99',
-      limite: 2500.00,
-      gerente: '01',
-      gerente_nome: 'José',
-      gerente_email: 'jose@gmail.com'
-    },
-    {
-      cpf: '222.222.222.22',
-      nome: 'João maria',
-      email: 'joaozinho@gmail.com',
-      endereco: {
-        tipo: 'Avenida',
-        logradouro: 'Rua das Flores',
-        numero: '56',
-        complemento: 'ap09',
-        cep: '88888-888',
-        cidade: 'Curitiba',
-        estado: 'PR'
-      },
-      conta: '3333',
-      saldo: '9999.99',
-      limite: 2500.00,
-      gerente: '01',
-      gerente_nome: 'José',
-      gerente_email: 'jose@gmail.com'
-    },
-    {
-      cpf: '333.333.333.33',
-      nome: 'Ricardão',
-      email: 'ricardao@gmail.com',
-      endereco: {
-        tipo: 'Avenida',
-        logradouro: 'Rua das Flores',
-        numero: '56',
-        complemento: 'ap09',
-        cep: '88888-888',
-        cidade: 'Curitiba',
-        estado: 'PR'
-      },
-      conta: '5555',
-      saldo: '9999.99',
-      limite: 2500.00,
-      gerente: '01',
-      gerente_nome: 'José',
-      gerente_email: 'jose@gmail.com'
-    }
-  ]
+      error: (err) => {
+        alert('Erro ao carregar melhores clientes: ' + (err.error?.message || err.message || 'Unknown error'));
+      }
+    });
+  }
 }
