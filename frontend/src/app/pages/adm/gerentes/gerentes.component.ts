997b3b6a513198b38f7dd380e86aa8f88c74a8c8
Yas Tainá <yas.taina.silva@gmail.com>
2025-09-26 22:30:57 -0300

    Ajustes adm auth

--- frontend/src/app/pages/adm/gerentes/gerentes.component.ts
+++ frontend/src/app/pages/adm/gerentes/gerentes.component.ts
@@ -3,11 +3,12 @@ import { LocalGerentesService } from '../../../services';
 import { Gerente } from '../../../services/local/models/gerente';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-gerentes',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, NgxMaskPipe],
   templateUrl: './gerentes.component.html',
   styleUrls: ['./gerentes.component.css']
 })
