fdd6546eb60bbde6a950f0399f444b875dc45356
Yasmin Tainá da Silva <37520858+Yas-Taina@users.noreply.github.com>
2025-11-23 14:56:37 -0300

    Delete bantads-mock directory

--- bantads-mock/src/main/java/com/example/bantads/dto/LoginInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.bantads.dto;
-public class LoginInfo {
-    private String email;
-    private String senha;
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getSenha() { return senha; }
-    public void setSenha(String senha) { this.senha = senha; }
-}
